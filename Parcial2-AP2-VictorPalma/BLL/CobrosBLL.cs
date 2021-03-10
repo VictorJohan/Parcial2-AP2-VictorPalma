952528334db509b1a9f1a62e57cdecbb1db4261c
Victor Johan <Vicjohan23@gmail.com>
2021-03-10 19:04:57 -0400

    CONTROL Se programaron los metodos de la BLL

--- Parcial2-AP2-VictorPalma/BLL/CobrosBLL.cs
+++ Parcial2-AP2-VictorPalma/BLL/CobrosBLL.cs
@@ -1,11 +1,115 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore;
+using Parcial2_AP2_VictorPalma.DAL;
+using Parcial2_AP2_VictorPalma.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Linq.Expressions;
 using System.Threading.Tasks;
 
 namespace Parcial2_AP2_VictorPalma.BLL
 {
     public class CobrosBLL
     {
+        private Contexto _contexto { get; set; }
+
+        public CobrosBLL(Contexto contexto)
+        {
+            this._contexto = contexto;
+        }
+
+        public async Task<bool> Guardar(Cobros cobro)
+        {
+            bool ok = false;
+
+            try
+            {
+                if (!await _contexto.Cobro.AnyAsync(c => c.CobroId == cobro.CobroId))
+                {
+                    _contexto.Entry(cobro.Cliente).State = EntityState.Modified;
+                    _contexto.Cobro.Add(cobro);
+                    ok = await _contexto.SaveChangesAsync() > 0;
+                }
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+
+            return ok;
+        }
+
+        public async Task<Cobros> Buscar(int id)
+        {
+            Cobros cobro;
+
+            try
+            {
+                cobro = await _contexto.Cobro
+                    .Where(c => c.CobroId == id)
+                    .Include(c => c.Cliente)
+                    .Include(c => c.CobrosDetalle)
+                    .AsNoTracking()
+                    .SingleOrDefaultAsync();
+
+                var entidad = _contexto
+                .Set<Cobros>()
+                .Local.SingleOrDefault(c => c.CobroId == id);
+
+                if (entidad != null)
+                {
+                    _contexto.Entry(entidad).State = EntityState.Detached;
+                }
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+
+            return cobro;
+        }
+
+        public async Task<bool> Eliminar(int id)
+        {
+            bool ok = false;
+
+            try
+            {
+                var registro = await Buscar(id);
+                if(registro != null)
+                {
+                    _contexto.Cobro.Remove(registro);
+                    ok = await _contexto.SaveChangesAsync() > 0;
+                }
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+
+            return ok;
+        }
+
+
+        public async Task<List<Cobros>> GetCobros(Expression<Func<Cobros, bool>> criterio)
+        {
+            List<Cobros> lista = new List<Cobros>();
+
+            try
+            {
+                lista = await _contexto.Cobro.Where(criterio).Include(c => c.Cliente).Include(c => c.CobrosDetalle).ToListAsync();
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+
+            return lista;
+        }
+
     }
 }
--- Parcial2-AP2-VictorPalma/BLL/VentasBLL.cs
+++ Parcial2-AP2-VictorPalma/BLL/VentasBLL.cs
@@ -1,11 +1,154 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore;
+using Parcial2_AP2_VictorPalma.DAL;
+using Parcial2_AP2_VictorPalma.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Linq.Expressions;
 using System.Threading.Tasks;
 
 namespace Parcial2_AP2_VictorPalma.BLL
 {
     public class VentasBLL
     {
+        public Contexto _contexto { get; set; }
+
+        public VentasBLL(Contexto contexto)
+        {
+            this._contexto = contexto;
+        }
+
+        public async Task<bool> Guardar(Ventas venta)
+        {
+            if (!await Existe(venta.VentaId))
+                return await Insertar(venta);
+            else
+                return await Modificar(venta);
+        }
+
+        private async Task<bool> Existe(int id)
+        {
+            bool ok = false;
+
+            try
+            {
+                ok = await _contexto.Venta.AnyAsync(v => v.VentaId == id);
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+
+            return ok;
+        }
+
+        private async Task<bool> Insertar(Ventas venta)
+        {
+            bool ok = false;
+
+            try
+            {
+                _contexto.Venta.Add(venta);
+                ok = await _contexto.SaveChangesAsync() > 0;
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+
+            return ok;
+        }
+
+        private async Task<bool> Modificar(Ventas venta)
+        {
+            bool ok = false;
+
+            try
+            {
+                _contexto.Entry(venta.Cliente).State = EntityState.Modified;
+                _contexto.Entry(venta).State = EntityState.Modified;
+
+                ok = await _contexto.SaveChangesAsync() > 0;
+
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+
+            return ok;
+        }
+
+        private async Task<Ventas> Buscar(int id)
+        {
+            Ventas venta;
+
+            try
+            {
+                venta = await _contexto.Venta
+                   .Where(v => v.VentaId == id)
+                   .Include(v => v.Cliente)
+                   .AsNoTracking()
+                   .SingleOrDefaultAsync();
+
+                var entidad = _contexto
+                .Set<Ventas>()
+                .Local.SingleOrDefault(v => v.VentaId == id);
+
+                if (entidad != null)
+                {
+                    _contexto.Entry(entidad).State = EntityState.Detached;
+                }
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+
+            return venta;
+        }
+
+        public async Task<bool> Eliminar(int id)
+        {
+            bool ok = false;
+            try
+            {
+                var registro = await Buscar(id);
+                if(registro != null)
+                {
+                    _contexto.Venta.Remove(registro);
+                    ok = await _contexto.SaveChangesAsync() > 0;
+                }
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+
+            return ok;
+        }
+
+        public async Task<List<Ventas>> GetList(Expression<Func<Ventas, bool>> criterio)
+        {
+            List<Ventas> lista = new List<Ventas>();
+
+            try
+            {
+                lista = await _contexto.Venta.Where(criterio).Include(c => c.Cliente).ToListAsync();
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+
+            return lista;
+        }
+
     }
 }
