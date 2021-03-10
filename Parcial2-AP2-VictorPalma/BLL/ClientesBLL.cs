b4e51455a9ac4c237d5448dbb8358d49b4777889
Victor Johan <Vicjohan23@gmail.com>
2021-03-10 19:36:13 -0400

    Se creo la BLL ClientesBLL

---
+++ Parcial2-AP2-VictorPalma/BLL/ClientesBLL.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Parcial2_AP2_VictorPalma.DAL;
+using Parcial2_AP2_VictorPalma.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+
+namespace Parcial2_AP2_VictorPalma.BLL
+{
+    public class ClientesBLL
+    {
+        private Contexto _contexto { get; set; }
+
+        public ClientesBLL(Contexto contexto)
+        {
+            this._contexto = contexto;
+        }
+
+        public async Task<List<Clientes>> GetList(Expression<Func<Clientes, bool>> criterio)
+        {
+            List<Clientes> lista = new List<Clientes>();
+
+            try
+            {
+                lista = await _contexto.Cliente.Where(criterio).ToListAsync();
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+
+            return lista;
+        }
+    }
+}
